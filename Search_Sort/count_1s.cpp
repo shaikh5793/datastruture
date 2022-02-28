c20f53e4c9549aa3eef05cc202be85072a39f50a
Shaikh Shehajadali <shaikh5793.ali@gmail.com>
2022-02-28 11:44:17 +0530

    Data_structure code

--- Search_Sort/count_1s.cpp
+++ Search_Sort/count_1s.cpp
@@ -1,3 +1,5 @@
+//  count 1s in array 
+
 #include<iostream>
 using namespce std;
 int count_1s(int a[],int n)
@@ -14,20 +16,20 @@ int count_1s(int a[],int n)
 	  {
 	     if(mid == 0||a[mid-1] == 0)
 		   return (n-mid);
-		 else
-           high = mid -1;		 
+             else
+                   high = mid -1;		 
 	  }
    }
    return -1;
 }
 int  main()
 {
-  int  a[]={0,0,0,1,1,1,1};
-  int  n = sizeof(a)/sizeof(a[0];
-  int res = count_1s(a,n);
-  if(res == -1)
-    cout<<"number of 1s not allowed"<<endl;
-  else
-   cout<<"res : "<<res<<endl;  
+    int  a[]={0,0,0,1,1,1,1};
+    int  n = sizeof(a)/sizeof(a[0];
+    int res = count_1s(a,n);
+    if(res == -1)
+       cout<<"number of 1s not allowed"<<endl;
+    else
+       cout<<"res : "<<res<<endl;  
 
 }
